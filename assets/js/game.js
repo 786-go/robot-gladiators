5a134a10f46f62c4da58bcdc6766217c831c1573
786-go <noorullahw@gmail.com>
2022-01-20 14:15:15 -0800

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,38 +2,69 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
-var enemyAttack = vb12;
+var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    
+    // if player chooses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // update health
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " 
         + enemyHealth + " health remaining."
     );
 
+    // check enemy's health
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died.");
     }
     else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
-
+    // update health    
     playerHealth = playerHealth - enemyAttack;
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " 
         + playerHealth + " health remaining."
     );
-
+    // check player's health
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
     }
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+    }
+
+    // if player chooses to skip
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        //if yes, leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 2;
+        }
+        //if no, re-ask original question to fight
+        else {
+            fight();
+        }
+    }
+    else {
+        window.alert("You need to choose a valid option. Try again!");
+    }
+
+    
+
+    
+    
 
 };
 
