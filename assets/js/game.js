fe8d6817657e3de7cac3a7ae05e2376ce251f64a
786-go <noorullahw@gmail.com>
2022-01-23 13:30:49 -0800

    previous edits weren't saved

--- assets/js/game.js
+++ assets/js/game.js
@@ -86,6 +86,10 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
 
 
