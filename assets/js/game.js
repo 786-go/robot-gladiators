2050edc0c074a9e8796b819163db4586acd5b04a
786-go <noorullahw@gmail.com>
2022-01-28 11:51:25 -0800

    randomized fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,13 +22,17 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
+  var isPlayerTurn = true;
+  if(Math.random() > .5) {
+    isPlayerTurn = false;
+  }
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or run
-    if (fightOrSkip()) {
-      break;
-    } 
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
+    if (isPlayerTurn) {
+      if (fightOrSkip()) {
+        break;
+      }
+    
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
@@ -50,6 +54,8 @@ var fight = function(enemy) {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
 
+  } else {
+   
     // remove players's health by subtracting the amount set in the enemyAttack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
@@ -67,6 +73,8 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  isPlayerTurn = !isPlayerTurn;
+}
 };
 
 // function to start a new game
